ae1a324558c34224a74c61f0c84e9303524282ce
vnoksi <104561169+vnoksi@users.noreply.github.com>
2022-05-10 13:34:59 +0200

    Create leadFormCommon.js

---
+++ leadFormCommon.js
@@ -0,0 +1,50 @@
+{
+  "formElts": [
+    {
+      "id": "assignedTo",
+      "name": "assignedTo",
+      "type": "select",
+      "label": "Assigné à",
+      "value": "",
+      "config": {
+        "options": [
+          {
+            "label": "commercial 1",
+            "value": "commercial1"
+          },
+          {
+            "label": "commercial 2",
+            "value": "commercial2"
+          },
+          {
+            "label": "commercial 3",
+            "value": "commercial3"
+          },
+        ]
+      }
+    },
+    {
+      "id": "leadOrigin",
+      "name": "leadOrigin",
+      "type": "select",
+      "label": "Origine du contact",
+      "value": "",
+      "config": {
+        "options": [
+          {
+            "label": "origine 1",
+            "value": "commercial1"
+          },
+          {
+            "label": "origine 2",
+            "value": "commercial2"
+          },
+          {
+            "label": "origine 2",
+            "value": "commercial3"
+          },
+        ]
+      }
+    },
+  ]
+}
